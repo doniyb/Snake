96428fb8402fa2ef481e3159704efe953396cc8a
doniyb <158499638+doniyb@users.noreply.github.com>
2024-08-18 19:46:38 +0300

    Add files via upload

--- Snake_git/MyForm.cpp
+++ Snake_git/MyForm.cpp
@@ -1,4 +1,7 @@
 #include "MyForm.h"
+#include <cmath>
+#include <ctime>
+
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -11,4 +14,289 @@ int main(array<String^>^ args_main)
 	Application::Run(% form);
 	return 0;
 }
+
+struct Vector2 {
+	int x, y;
+};
+
+Vector2 direction;
+Vector2 positionFruitd;
+Vector2 GameArena;
+
+
+Snakegit::MyForm::MyForm(void)
+{
+	InitializeComponent();
+
+	GameArena.x = 400;
+	GameArena.y = 400;
+
+	OneLaunch = true;
+	Play();
+}
+
+void Snakegit::MyForm::GeneralPosintionFruit()
+{
+	Random^ rand = gcnew Random();
+	positionFruitd.x = rand->Next(100, GameArena.x);
+	positionFruitd.y = rand->Next(100, GameArena.y);
+
+
+	for (int i = 0; i < score; ++i) {
+		if (positionFruitd.x == Snake[i]->Location.X && positionFruitd.y == Snake[i]->Location.Y) {
+			positionFruitd.x = 130;
+			positionFruitd.y = 279;
+			break;
+		}
+	}
+
+	int tempX = positionFruitd.x % step;
+	int tempY = positionFruitd.y % step;
+
+	fruit->Location = Point(positionFruitd.x, positionFruitd.y);
+
+	if (fruit->Location.X >= 390 || fruit->Location.X <= 20) {
+		fruit->Location = Point(150, 200);
+	}
+
+	if (fruit->Location.Y >= 400 || fruit->Location.Y <= 150) {
+		fruit->Location = Point(200, 300);
+	}
+
+	this->Controls->Add(fruit);
+
+}
+
+void Snakegit::MyForm::Eating()
+{
+	if (abs(Snake[0]->Location.X - fruit->Location.X) <= 5 &&
+		abs(Snake[0]->Location.Y - fruit->Location.Y) <= 5) {
+		ScoreGame->Text="Score:"+ ++score;
+
+		
+		Snake[score] = gcnew PictureBox();
+		Snake[score]->Location = Point(Snake[score - 1]->Location.X + step * direction.x, Snake[score - 1]->Location.Y - step * direction.y);
+		Snake[score]->BackColor = Color::Green;
+		Snake[score]->Width = step;
+		Snake[score]->Height = step;
+		this->Controls->Add(Snake[score]);
+
+		GeneralPosintionFruit();
+	}
+}
+
+void Snakegit::MyForm::Movement()
+{
+	for (int i = score; i >= 1; --i)
+		Snake[i]->Location = Snake[i - 1]->Location;
+
+	Snake[0]->Location = Point(Snake[0]->Location.X + direction.x * step, Snake[0] -> Location.Y + direction.y * step);
+}
+
+void Snakegit::MyForm::EatYur()
+{
+	for (int i = 1; i < score; ++i) {
+		if (Snake[0]->Location == Snake[i]->Location) {
+			GameOver();
+		}
+	}
+}
+
+void Snakegit::MyForm::GameOver()
+{
+	play = true;
+	die = true;
+	OneLaunch = true;
+	Snake[0]->Location = Point(1000, 1000);
+	fruit ->Location = Point(1200, 1200);
+	for (int i = 1; i <= score; ++i) {
+		Snake[i]->Location = Point(1000+i, 1000+i);
+	}
+
+}
+
+void Snakegit::MyForm::Play()
+{
+	if (!OneLaunch) {
+		this->Controls->Remove(fruit);
+
+		for (int i = 0; i < score; ++i) {
+			this->Controls->Remove(Snake[i]);
+		}
+
+
+		score = 0;
+	}
+
+	else {
+		OneLaunch = false;
+
+		Snake = gcnew array <PictureBox^, 1>(400);
+		Snake[0] = gcnew PictureBox();
+		Snake[0]->Location = Point(200, 200);
+		Snake[0]->BackColor = Color::Red;
+		Snake[0]->Width = step;
+		Snake[0]->Height = step;
+
+		score = 0;
+		this->Controls->Add(Snake[0]);
+
+		fruit = gcnew PictureBox();
+		fruit->BackColor = Color::Purple;
+		fruit->Width = step;
+		fruit->Height = step;
+		GeneralPosintionFruit();
+
+		timer->Interval = updateInterval;
+		timer->Start();
+
+		direction.x = 1;
+		direction.y = 0;
+
+		play = true;
+		die = false;
+
+		ScoreGame->Text = "Score: 0";
+
+		groupBoxSeting->Visible = false;
+	}
+}
+
+void Snakegit::MyForm::ChackBorders()
+{
+	if (Snake[0]->Location.X >= 400 || Snake[0]->Location.X <= 10) {
+		GameOver();
+	}
+
+	if (Snake[0]->Location.Y >= 415 || Snake[0]->Location.Y <= 127) {
+		GameOver();
+	}
+}
+
+
+
+System::Void Snakegit::MyForm::Alpy_Click(System::Object^ sender, System::EventArgs^ e)
+{
+
+	updateInterval = Convert::ToSingle(UpDownSpeedSnake->Value);
+	timer->Interval = updateInterval;
+
+	Alpy->Enabled = false;
+	UpDownSpeedSnake->Enabled = false;
+	groupBoxSeting->Visible = false;
+
+	play = true;
+	timer->Start();
+	return System::Void();
+}
+
+System::Void Snakegit::MyForm::menuToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	if (groupBoxSeting->Visible == false) {
+		play = false;
+
+		Alpy->Enabled = true;
+		UpDownSpeedSnake->Enabled = true;
+		groupBoxSeting->Visible = true;
+	}
+
+	else {
+		play = true;
+		timer->Start();
+
+		Alpy->Enabled = false;
+		UpDownSpeedSnake->Enabled = false;
+		groupBoxSeting->Visible = false;
+	}
+}
+
+System::Void Snakegit::MyForm::newGameToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	Play();
+	return System::Void();
+}
+
+System::Void Snakegit::MyForm::pauseToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	if (play) {
+		play = false;
+	}
+
+	else {
+		play = true;
+		timer->Start();
+	}
+
+	return System::Void();
+}
+
+System::Void Snakegit::MyForm::informationToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	MessageBox::Show("Вправо\tстрелка вправо\nВлево\tстрелка влево\nВверх\tстрелка вверх\nВниз\tстрелка вниз");
+
+	return System::Void();
+}
+
+System::Void Snakegit::MyForm::exaitToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	Application::Exit();
+}
+
+System::Void Snakegit::MyForm::MyForm_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
+{
+	if ((e->KeyCode.ToString()=="Right" && score ==0)|| 
+		(e->KeyCode.ToString() == "Right" && direction.x != -1 && direction.y != 0)) {
+		direction.x = 1;
+		direction.y = 0;
+	}
+
+	if ((e->KeyCode.ToString() == "Left" && score == 0) ||
+		(e->KeyCode.ToString() == "Left" && direction.x != 1 && direction.y != 0)) {
+		direction.x = -1;
+		direction.y = 0;
+	}
+
+	if ((e->KeyCode.ToString() == "Down" && score == 0) ||
+		(e->KeyCode.ToString() == "Down" && direction.x != 0 && direction.y != -1)) {
+		direction.x = 0;
+		direction.y = 1;
+	}
+
+	if ((e->KeyCode.ToString() == "Up" && score == 0) ||
+		(e->KeyCode.ToString() == "Up" && direction.x != 0 && direction.y != 1)) {
+		direction.x = 0;
+		direction.y = -1;
+	}
+
+
+	return System::Void();
+}
+
+void Snakegit::MyForm::GameForm_Update(Object^ obgect, EventArgs^ e)
+{
+	if (!die && play) {
+		Movement();
+
+		Eating();
+		EatYur();
+		ChackBorders();
+	}
+
+	else if(die && play){
+		timer->Stop();
+		MessageBox::Show("Stop");
+	}
+
+	else if (!die && !play) {
+		timer->Stop();
+		MessageBox::Show("Pause");
+	}	
+}
+
+System::Void Snakegit::MyForm::groupBoxSeting_Enter(System::Object^ sender, System::EventArgs^ e)
+{
+	
+}
+
+
 
--- Snake_git/MyForm.h
+++ Snake_git/MyForm.h
@@ -15,13 +15,7 @@ namespace Snakegit {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
-		MyForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
-		}
+		MyForm(void);
 
 	protected:
 		/// <summary>
@@ -43,7 +37,8 @@ namespace Snakegit {
 	private: System::Windows::Forms::ToolStripMenuItem^ informationToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ exaitToolStripMenuItem;
 	private: System::Windows::Forms::GroupBox^ GameInformation;
-	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ ScoreGame;
+
 	private: System::Windows::Forms::GroupBox^ groupBoxSeting;
 	private: System::Windows::Forms::Button^ ButtonSpeedSnake;
 
@@ -56,7 +51,9 @@ namespace Snakegit {
 
 	private: System::Windows::Forms::PictureBox^ WallUp;
 	private: System::Windows::Forms::PictureBox^ WallLow;
-	private: System::Windows::Forms::Timer^ timer1;
+	private: System::Windows::Forms::Timer^ timer;
+	private: System::Windows::Forms::Button^ Alpy;
+
 	private: System::ComponentModel::IContainer^ components;
 
 
@@ -75,6 +72,7 @@ namespace Snakegit {
 		void InitializeComponent(void)
 		{
 			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->MenuRama = (gcnew System::Windows::Forms::MenuStrip());
 			this->menuToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->newGameToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
@@ -83,16 +81,17 @@ namespace Snakegit {
 			this->informationToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->exaitToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->GameInformation = (gcnew System::Windows::Forms::GroupBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->ScoreGame = (gcnew System::Windows::Forms::Label());
 			this->groupBoxSeting = (gcnew System::Windows::Forms::GroupBox());
-			this->LabelSpeed = (gcnew System::Windows::Forms::Label());
-			this->UpDownSpeedSnake = (gcnew System::Windows::Forms::NumericUpDown());
+			this->Alpy = (gcnew System::Windows::Forms::Button());
 			this->ButtonSpeedSnake = (gcnew System::Windows::Forms::Button());
+			this->UpDownSpeedSnake = (gcnew System::Windows::Forms::NumericUpDown());
+			this->LabelSpeed = (gcnew System::Windows::Forms::Label());
 			this->WallLeft = (gcnew System::Windows::Forms::PictureBox());
 			this->WallRight = (gcnew System::Windows::Forms::PictureBox());
 			this->WallUp = (gcnew System::Windows::Forms::PictureBox());
 			this->WallLow = (gcnew System::Windows::Forms::PictureBox());
-			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->timer = (gcnew System::Windows::Forms::Timer(this->components));
 			this->MenuRama->SuspendLayout();
 			this->GameInformation->SuspendLayout();
 			this->groupBoxSeting->SuspendLayout();
@@ -112,7 +111,8 @@ namespace Snakegit {
 			});
 			this->MenuRama->Location = System::Drawing::Point(0, 0);
 			this->MenuRama->Name = L"MenuRama";
-			this->MenuRama->Size = System::Drawing::Size(559, 28);
+			this->MenuRama->Padding = System::Windows::Forms::Padding(4, 2, 0, 2);
+			this->MenuRama->Size = System::Drawing::Size(419, 24);
 			this->MenuRama->TabIndex = 0;
 			this->MenuRama->Text = L"menuStrip1";
 			// 
@@ -123,141 +123,175 @@ namespace Snakegit {
 					this->setingToolStripMenuItem, this->pauseToolStripMenuItem
 			});
 			this->menuToolStripMenuItem->Name = L"menuToolStripMenuItem";
-			this->menuToolStripMenuItem->Size = System::Drawing::Size(60, 24);
+			this->menuToolStripMenuItem->Size = System::Drawing::Size(50, 20);
 			this->menuToolStripMenuItem->Text = L"Menu";
+			this->menuToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::menuToolStripMenuItem_Click);
 			// 
 			// newGameToolStripMenuItem
 			// 
 			this->newGameToolStripMenuItem->Name = L"newGameToolStripMenuItem";
-			this->newGameToolStripMenuItem->Size = System::Drawing::Size(164, 26);
+			this->newGameToolStripMenuItem->Size = System::Drawing::Size(180, 22);
 			this->newGameToolStripMenuItem->Text = L"New game";
+			this->newGameToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::newGameToolStripMenuItem_Click);
 			// 
 			// setingToolStripMenuItem
 			// 
 			this->setingToolStripMenuItem->Name = L"setingToolStripMenuItem";
-			this->setingToolStripMenuItem->Size = System::Drawing::Size(164, 26);
+			this->setingToolStripMenuItem->Size = System::Drawing::Size(180, 22);
 			this->setingToolStripMenuItem->Text = L"seting";
 			// 
 			// pauseToolStripMenuItem
 			// 
 			this->pauseToolStripMenuItem->Name = L"pauseToolStripMenuItem";
-			this->pauseToolStripMenuItem->Size = System::Drawing::Size(164, 26);
+			this->pauseToolStripMenuItem->Size = System::Drawing::Size(180, 22);
 			this->pauseToolStripMenuItem->Text = L"pause";
+			this->pauseToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::pauseToolStripMenuItem_Click);
 			// 
 			// informationToolStripMenuItem
 			// 
 			this->informationToolStripMenuItem->Name = L"informationToolStripMenuItem";
-			this->informationToolStripMenuItem->Size = System::Drawing::Size(101, 24);
+			this->informationToolStripMenuItem->Size = System::Drawing::Size(82, 20);
 			this->informationToolStripMenuItem->Text = L"Information";
+			this->informationToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::informationToolStripMenuItem_Click);
 			// 
 			// exaitToolStripMenuItem
 			// 
 			this->exaitToolStripMenuItem->Name = L"exaitToolStripMenuItem";
-			this->exaitToolStripMenuItem->Size = System::Drawing::Size(55, 24);
+			this->exaitToolStripMenuItem->Size = System::Drawing::Size(44, 20);
 			this->exaitToolStripMenuItem->Text = L"exait";
+			this->exaitToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::exaitToolStripMenuItem_Click);
 			// 
 			// GameInformation
 			// 
-			this->GameInformation->Controls->Add(this->label1);
-			this->GameInformation->Location = System::Drawing::Point(13, 34);
+			this->GameInformation->Controls->Add(this->ScoreGame);
+			this->GameInformation->Location = System::Drawing::Point(10, 28);
+			this->GameInformation->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->GameInformation->Name = L"GameInformation";
-			this->GameInformation->Size = System::Drawing::Size(200, 100);
+			this->GameInformation->Padding = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->GameInformation->Size = System::Drawing::Size(150, 81);
 			this->GameInformation->TabIndex = 1;
 			this->GameInformation->TabStop = false;
 			this->GameInformation->Text = L"game information";
 			// 
-			// label1
+			// ScoreGame
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(28, 41);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(56, 16);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"Score: 0";
+			this->ScoreGame->AutoSize = true;
+			this->ScoreGame->Location = System::Drawing::Point(21, 33);
+			this->ScoreGame->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->ScoreGame->Name = L"ScoreGame";
+			this->ScoreGame->Size = System::Drawing::Size(47, 13);
+			this->ScoreGame->TabIndex = 0;
+			this->ScoreGame->Text = L"Score: 0";
 			// 
 			// groupBoxSeting
 			// 
+			this->groupBoxSeting->Controls->Add(this->Alpy);
 			this->groupBoxSeting->Controls->Add(this->ButtonSpeedSnake);
 			this->groupBoxSeting->Controls->Add(this->UpDownSpeedSnake);
 			this->groupBoxSeting->Controls->Add(this->LabelSpeed);
-			this->groupBoxSeting->Location = System::Drawing::Point(244, 34);
+			this->groupBoxSeting->Location = System::Drawing::Point(183, 28);
+			this->groupBoxSeting->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->groupBoxSeting->Name = L"groupBoxSeting";
-			this->groupBoxSeting->Size = System::Drawing::Size(200, 100);
+			this->groupBoxSeting->Padding = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->groupBoxSeting->Size = System::Drawing::Size(197, 81);
 			this->groupBoxSeting->TabIndex = 2;
 			this->groupBoxSeting->TabStop = false;
 			this->groupBoxSeting->Text = L"Seting";
 			this->groupBoxSeting->Visible = false;
+			this->groupBoxSeting->Enter += gcnew System::EventHandler(this, &MyForm::groupBoxSeting_Enter);
 			// 
-			// LabelSpeed
+			// Alpy
 			// 
-			this->LabelSpeed->AutoSize = true;
-			this->LabelSpeed->Location = System::Drawing::Point(28, 47);
-			this->LabelSpeed->Name = L"LabelSpeed";
-			this->LabelSpeed->Size = System::Drawing::Size(127, 16);
-			this->LabelSpeed->TabIndex = 0;
-			this->LabelSpeed->Text = L"Output speed snake";
-			// 
-			// UpDownSpeedSnake
-			// 
-			this->UpDownSpeedSnake->Location = System::Drawing::Point(31, 78);
-			this->UpDownSpeedSnake->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
-			this->UpDownSpeedSnake->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
-			this->UpDownSpeedSnake->Name = L"UpDownSpeedSnake";
-			this->UpDownSpeedSnake->Size = System::Drawing::Size(120, 22);
-			this->UpDownSpeedSnake->TabIndex = 1;
-			this->UpDownSpeedSnake->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->Alpy->Location = System::Drawing::Point(104, 38);
+			this->Alpy->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Alpy->Name = L"Alpy";
+			this->Alpy->Size = System::Drawing::Size(56, 19);
+			this->Alpy->TabIndex = 3;
+			this->Alpy->Text = L"alpy\r\n";
+			this->Alpy->UseVisualStyleBackColor = true;
+			this->Alpy->Click += gcnew System::EventHandler(this, &MyForm::Alpy_Click);
 			// 
 			// ButtonSpeedSnake
 			// 
-			this->ButtonSpeedSnake->Location = System::Drawing::Point(31, 107);
+			this->ButtonSpeedSnake->Location = System::Drawing::Point(23, 87);
+			this->ButtonSpeedSnake->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->ButtonSpeedSnake->Name = L"ButtonSpeedSnake";
-			this->ButtonSpeedSnake->Size = System::Drawing::Size(75, 23);
+			this->ButtonSpeedSnake->Size = System::Drawing::Size(56, 19);
 			this->ButtonSpeedSnake->TabIndex = 2;
 			this->ButtonSpeedSnake->Text = L"Alpy";
 			this->ButtonSpeedSnake->UseVisualStyleBackColor = true;
 			// 
+			// UpDownSpeedSnake
+			// 
+			this->UpDownSpeedSnake->Location = System::Drawing::Point(104, 15);
+			this->UpDownSpeedSnake->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->UpDownSpeedSnake->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1000, 0, 0, 0 });
+			this->UpDownSpeedSnake->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			this->UpDownSpeedSnake->Name = L"UpDownSpeedSnake";
+			this->UpDownSpeedSnake->Size = System::Drawing::Size(90, 20);
+			this->UpDownSpeedSnake->TabIndex = 1;
+			this->UpDownSpeedSnake->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 1, 0, 0, 0 });
+			// 
+			// LabelSpeed
+			// 
+			this->LabelSpeed->AutoSize = true;
+			this->LabelSpeed->Location = System::Drawing::Point(4, 15);
+			this->LabelSpeed->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->LabelSpeed->Name = L"LabelSpeed";
+			this->LabelSpeed->Size = System::Drawing::Size(103, 13);
+			this->LabelSpeed->TabIndex = 0;
+			this->LabelSpeed->Text = L"Output speed snake";
+			// 
 			// WallLeft
 			// 
 			this->WallLeft->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->WallLeft->Location = System::Drawing::Point(0, 140);
+			this->WallLeft->Location = System::Drawing::Point(0, 114);
+			this->WallLeft->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->WallLeft->Name = L"WallLeft";
-			this->WallLeft->Size = System::Drawing::Size(15, 400);
+			this->WallLeft->Size = System::Drawing::Size(11, 325);
 			this->WallLeft->TabIndex = 3;
 			this->WallLeft->TabStop = false;
 			// 
 			// WallRight
 			// 
 			this->WallRight->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->WallRight->Location = System::Drawing::Point(544, 140);
+			this->WallRight->Location = System::Drawing::Point(408, 114);
+			this->WallRight->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->WallRight->Name = L"WallRight";
-			this->WallRight->Size = System::Drawing::Size(15, 400);
+			this->WallRight->Size = System::Drawing::Size(11, 325);
 			this->WallRight->TabIndex = 4;
 			this->WallRight->TabStop = false;
 			// 
 			// WallUp
 			// 
 			this->WallUp->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->WallUp->Location = System::Drawing::Point(0, 141);
+			this->WallUp->Location = System::Drawing::Point(0, 114);
+			this->WallUp->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->WallUp->Name = L"WallUp";
-			this->WallUp->Size = System::Drawing::Size(559, 15);
+			this->WallUp->Size = System::Drawing::Size(419, 12);
 			this->WallUp->TabIndex = 5;
 			this->WallUp->TabStop = false;
 			// 
 			// WallLow
 			// 
 			this->WallLow->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->WallLow->Location = System::Drawing::Point(0, 525);
+			this->WallLow->Location = System::Drawing::Point(0, 427);
+			this->WallLow->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->WallLow->Name = L"WallLow";
-			this->WallLow->Size = System::Drawing::Size(559, 15);
+			this->WallLow->Size = System::Drawing::Size(419, 12);
 			this->WallLow->TabIndex = 6;
 			this->WallLow->TabStop = false;
 			// 
+			// timer
+			// 
+			this->timer->Tick += gcnew System::EventHandler(this, &MyForm::GameForm_Update);
+			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::AppWorkspace;
-			this->ClientSize = System::Drawing::Size(559, 539);
+			this->ClientSize = System::Drawing::Size(419, 438);
 			this->Controls->Add(this->WallLow);
 			this->Controls->Add(this->WallUp);
 			this->Controls->Add(this->WallRight);
@@ -265,10 +299,14 @@ namespace Snakegit {
 			this->Controls->Add(this->groupBoxSeting);
 			this->Controls->Add(this->GameInformation);
 			this->Controls->Add(this->MenuRama);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MainMenuStrip = this->MenuRama;
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->MaximizeBox = false;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Danina_the_Snake";
+			this->TransparencyKey = System::Drawing::Color::DimGray;
+			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::MyForm_KeyDown);
 			this->MenuRama->ResumeLayout(false);
 			this->MenuRama->PerformLayout();
 			this->GameInformation->ResumeLayout(false);
@@ -285,5 +323,39 @@ namespace Snakegit {
 
 		}
 #pragma endregion
-	};
+
+		private: PictureBox^ fruit;//фрукт
+		private: bool play;//начать
+		private: array<PictureBox^>^ Snake;//змея
+		private: bool die;//смерть
+		private: bool OneLaunch;//первый запуск
+		private: int step = 10;//шаг
+		private: int score = 0;//счет
+		private: double updateInterval = 100;//интервал обновления
+
+
+		private: void GeneralPosintionFruit();//генерация фрукта
+		private: void Eating();//кушать
+		private: void Movement();
+		private: void EatYur();//съел себя
+		private: void GameOver();
+		private: void Play();
+		private: void ChackBorders();
+
+
+
+
+private: System::Void Alpy_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void menuToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void newGameToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void pauseToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void informationToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void exaitToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e);
+
+
+private: System::Void MyForm_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e);
+	   
+private: void GameForm_Update(Object^ obgect, EventArgs^ e);
+private: System::Void groupBoxSeting_Enter(System::Object^ sender, System::EventArgs^ e);
+};
 }
